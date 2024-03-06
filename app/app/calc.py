42a936b7f97a60d5403c09441e1a9fdc87ae33e4
Vishal Parmar <vishalparmar6958@gmail.com>
2024-03-07 01:14:46 +0530

    Config DB and add wait_for_db command

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x, y):
+    """Add two numbers and return result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract two numbers and return result"""
+    return x - y
